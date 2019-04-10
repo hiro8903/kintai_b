0c7ed14093345cf26bd734384bc558e0be67996d
EC2 Default User <ec2-user@ip-172-31-34-10.us-east-2.compute.internal>
2019-04-10 16:24:53 +0000

    delete　basic info display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,5 @@ module UsersHelper
     def decimal_time(time)
       sprintf("%10.2f", ((time.hour * 60) + time.min)/60.0)
     end
-    
-    
-    
-   
-    # def format_basic_time(datetime)
-    # format("%.2f", ((datetime.hour * 60) + datetime.min)/60.0)
-    # end
+
 end
