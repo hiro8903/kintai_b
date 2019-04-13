5efe5dc74aba06d494f00c78eece6fc9fcde64df
EC2 Default User <ec2-user@ip-172-31-34-10.us-east-2.compute.internal>
2019-04-13 08:33:31 +0000

    edit routes.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   
     def decimal_time(datetime)
-      #sprintf("%10.2f", ((datetime.hour * 60) + datetime.min)/60.0)
-      format("%.2f", ((datetime.hour * 60) + datetime.min)/60.0)
+      sprintf("%10.2f", ((datetime.hour * 60) + datetime.min)/60.0)
+      #format("%.2f", ((datetime.hour * 60) + datetime.min)/60.0)
     end
 
 
